c5b1c0177c01341a36ee5cce012216be55a5bfb3
wataru <0028tkhr@gmail.com>
2025-06-20 11:44:29 +0900

    tailwind cssの導入

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   ssr: false,
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt','@nuxtjs/tailwindcss'],
   vite: {
     server: {
       allowedHosts: ['frontend'],
