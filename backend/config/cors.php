f5ca74dfedd39510acfe129ab8e23109686ce56c
wataru <0028tkhr@gmail.com>
2026-02-08 18:09:48 +0900

    cors修正

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,19 +1,9 @@
 <?php
 
-// return [
-//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-//     'allowed_methods' => ['*'],
-//     'allowed_origins' => ['http://localhost:9000'],
-//     'allowed_headers' => ['*'],
-//     'exposed_headers' => [],
-//     'max_age' => 0,
-//     'supports_credentials' => false,
-// ];
-
 return [
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
